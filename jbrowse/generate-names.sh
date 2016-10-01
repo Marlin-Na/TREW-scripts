33b8f1c7d99dd63c3c076e12f2139121d560161c
Marlin- <marlin-@gmx.cn>
2016-10-01 09:59:53 +0800

    Minor

--- jbrowse/generate-names.sh
+++ jbrowse/generate-names.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd ~/jbrowse
+# Path to jbrowse
+jbrowsedir=~/jbrowse
+
+cd $jbrowsedir
 bin/generate-names.pl --out data/hg19 --mem 50000000000 &&
 bin/generate-names.pl --out data/mm10 --mem 50000000000 &&
 bin/generate-names.pl --out data/dm6 --mem 50000000000
