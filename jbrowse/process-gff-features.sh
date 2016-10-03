09a507113331bf129a05a684fb35f8c85dc58844
Marlin- <marlin-@gmx.cn>
2016-10-03 12:12:15 +0800

    Process less col features as main tracks

--- jbrowse/process-gff-features.sh
+++ jbrowse/process-gff-features.sh
@@ -9,15 +9,15 @@ cd $jbrowsedir
 
 # Process gff files
 ## hg19
-bin/flatfile-to-json.pl --gff $gffdir/hg19_m6A.gff3 --trackLabel 'all_m6A_less_col' --out data/hg19 --trackType CanvasFeatures --key 'm6A (less col)' --sortMem 50000000000
-bin/flatfile-to-json.pl --gff $gffdir/hg19_m1A.gff3 --trackLabel 'all_m1A_less_col' --out data/hg19 --trackType CanvasFeatures --key 'm1A (less col)' --sortMem 50000000000
-bin/flatfile-to-json.pl --gff $gffdir/hg19_m5C.gff3 --trackLabel 'all_m5C_less_col' --out data/hg19 --trackType CanvasFeatures --key 'm5C (less col)' --sortMem 50000000000
-bin/flatfile-to-json.pl --gff $gffdir/hg19_Psi.gff3 --trackLabel 'all_Psi_less_col' --out data/hg19 --trackType CanvasFeatures --key 'Psi (less col)' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/hg19_m6A.gff3 --trackLabel 'all_m6A' --out data/hg19 --trackType CanvasFeatures --key 'All m6A sites' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/hg19_m1A.gff3 --trackLabel 'all_m1A' --out data/hg19 --trackType CanvasFeatures --key 'All m1A sites' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/hg19_m5C.gff3 --trackLabel 'all_m5C' --out data/hg19 --trackType CanvasFeatures --key 'All m5C sites' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/hg19_Psi.gff3 --trackLabel 'all_Psi' --out data/hg19 --trackType CanvasFeatures --key 'All Psi sites' --sortMem 50000000000
 ## mm10
-bin/flatfile-to-json.pl --gff $gffdir/mm10_m6A.gff3 --trackLabel 'all_m6A_less_col' --out data/mm10 --trackType CanvasFeatures --key 'm6A (less col)' --sortMem 50000000000
-bin/flatfile-to-json.pl --gff $gffdir/mm10_m5C.gff3 --trackLabel 'all_m5C_less_col' --out data/mm10 --trackType CanvasFeatures --key 'm5C (less col)' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/mm10_m6A.gff3 --trackLabel 'all_m6A' --out data/mm10 --trackType CanvasFeatures --key 'All m6A sites' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/mm10_m5C.gff3 --trackLabel 'all_m5C' --out data/mm10 --trackType CanvasFeatures --key 'All m5C sites' --sortMem 50000000000
 ## dm6
-bin/flatfile-to-json.pl --gff $gffdir/dm6_m6A.gff3 --trackLabel 'all_m6A_less_col' --out data/dm6 --trackType CanvasFeatures --key 'm6A (less col)' --sortMem 50000000000
+bin/flatfile-to-json.pl --gff $gffdir/dm6_m6A.gff3 --trackLabel 'all_m6A' --out data/dm6 --trackType CanvasFeatures --key 'All m6A sites' --sortMem 50000000000
 
 
 # Generate name index
--- jbrowse/remove-tracks.sh
+++ jbrowse/remove-tracks.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# Remove gff features with the name index
+# Remove all current tracks
 
 jbrowsedir=~/jbrowse
 
 cd $jbrowsedir
 
 ## hg19
-bin/remove-track.pl -D --trackLabel 'all_m6A_with_name' --dir data/hg19 
-bin/remove-track.pl -D --trackLabel 'all_m1A_with_name' --dir data/hg19 
-bin/remove-track.pl -D --trackLabel 'all_m5C_with_name' --dir data/hg19 
-bin/remove-track.pl -D --trackLabel 'all_Psi_with_name' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_m6A' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_m1A' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_m5C' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_Psi' --dir data/hg19 
 ## mm10
-bin/remove-track.pl -D --trackLabel 'all_m6A_with_name' --dir data/mm10 
-bin/remove-track.pl -D --trackLabel 'all_m5C_with_name' --dir data/mm10 
+bin/remove-track.pl -D --trackLabel 'all_m6A' --dir data/mm10 
+bin/remove-track.pl -D --trackLabel 'all_m5C' --dir data/mm10 
 ## dm6
-bin/remove-track.pl -D --trackLabel 'all_m6A_with_name' --dir data/dm6 
+bin/remove-track.pl -D --trackLabel 'all_m6A' --dir data/dm6 
+
+
+## hg19
+bin/remove-track.pl -D --trackLabel 'all_m6A_less_col' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_m1A_less_col' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_m5C_less_col' --dir data/hg19 
+bin/remove-track.pl -D --trackLabel 'all_Psi_less_col' --dir data/hg19 
+## mm10
+bin/remove-track.pl -D --trackLabel 'all_m6A_less_col' --dir data/mm10 
+bin/remove-track.pl -D --trackLabel 'all_m5C_less_col' --dir data/mm10 
+## dm6
+bin/remove-track.pl -D --trackLabel 'all_m6A_less_col' --dir data/dm6 
+
 
 
 # Sync to the server
